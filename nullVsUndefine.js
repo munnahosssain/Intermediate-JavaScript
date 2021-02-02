34a042b39224b1213945dfce00e95d09ba9386aa
Munna Hossain <mayinuddin.munna@gmail.com>
2021-02-02 23:28:07 +0600

    nullVsUndefined addded

---
+++ nullVsUndefine.js
@@ -0,0 +1,8 @@
+const data = { nam: 'Hossain', age: 22, phone: 01572, address: "#126/B" }
+console.log('data: ', data.office);
+
+let getNumber = [311, 273, 249];
+console.log('indexOF: ', getNumber[3]);
+
+let fun = undefined;
+console.log('undefined: ', fun);
